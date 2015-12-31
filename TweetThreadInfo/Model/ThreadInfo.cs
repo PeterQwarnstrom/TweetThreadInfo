629ae5e3b5d73b9e8ea83735a29218efb095dbe5
Peter Qwarnstrom <peter.qwarnstrom@gmail.com>
2015-12-31 01:27:03 +0100

    Started work

---
+++ TweetThreadInfo/Model/ThreadInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TweetThreadInfo.Model
+{
+    public class ThreadInfo
+    {
+        public int ThreadStartId { get; set; }
+
+        public List<Tag> Tags { get; set; }
+    }
+}
